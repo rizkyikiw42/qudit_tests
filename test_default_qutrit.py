867c3a2129bb9bf3eeb28259fab05ff60fb93aac
rizkyikiw42 <rizzikwann@gmail.com>
2022-06-02 16:42:35 -0700

    Adding test for 2 qutrits

--- test_default_qutrit.py
+++ test_default_qutrit.py
@@ -5,27 +5,39 @@ from pennylane.devices import DefaultQutrit
 from pennylane.ops.qutrit import QutritUnitary
 
 
-U = np.array([[0, 1, 0],
-              [0, 0, 1],
-              [1, 0, 0]])
-# U = np.multiply(1 / np.sqrt(2), U)
+shift = np.array(
+            [[0, 0, 1],
+             [1, 0, 0],
+             [0, 1, 0]])
 
 
 # dev = DefaultQutrit(wires=1)
-# op = QutritUnitary(U, wires=0)
+# op = QutritUnitary(shift, wires=0)
 # dev.apply([op])
 
 # print(dev.probability(wires=0))
 
-dev = qml.device('default.qutrit', wires=1)
+dev = qml.device('default.qutrit', wires=2)
 
 @qml.qnode(dev)
 def test(iters):
     for _ in range(iters):
-        qml.QutritUnitary(U, wires=0)
+        qml.QutritUnitary(shift, wires=0)
 
     return qml.probs(wires=0)
 
 
-for i in range(4):
-    print(test(i))
+# for i in range(4):
+#     print(test(i))
+
+
+@qml.qnode(dev)
+def test_swap():
+    qml.QutritUnitary(shift, wires=0)       # |00> -> |10>
+    qml.QutritUnitary(shift.T, wires=1)     # |10> -> |12>
+    qml.TSWAP(wires=[0, 1])                 # |12> -> |21>
+    qml.TAdd(wires=[0, 1])                  # |21> -> |20>
+
+    return qml.state()
+
+print(test_swap())
