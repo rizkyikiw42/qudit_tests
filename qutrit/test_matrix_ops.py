a64a9c5bdf5540c8d88c5b086f0364dd667791ef
rizkyikiw42 <rizzikwann@gmail.com>
2022-06-07 11:06:09 -0700

    Adding test files

---
+++ qutrit/test_matrix_ops.py
@@ -0,0 +1,21 @@
+import pytest
+import numpy as np
+from scipy.stats import unitary_group
+
+import pennylane as qml
+from pennylane.wires import Wires
+from pennylane.operation import DecompositionUndefinedError
+
+
+class TestQutritUnitary:
+    pass
+
+
+class TestControlledQutritUnitary:
+    pass
+
+
+class TestUnitaryLabels:
+    pass
+
+
