1b57c0a61cd669e0ccd9adb4ae5790e537376f44
rizkyikiw42 <rizzikwann@gmail.com>
2022-07-20 12:03:24 -0700

    Cleaning up

--- qutrit/test_qutrits.py
+++ qutrit/test_qutrits.py
@@ -33,38 +33,17 @@ U_tswap = np.array(
     dtype=np.complex128,
 )
 
-
-# dev = qml.device("default.qutrit", wires=2)
-
-dev2 = qml.device("default.qubit", wires=4, shots=[10, 5])
+dev = qml.device("default.qutrit", wires=2)
 
 
-# @qml.qnode(dev)
-# def test_qnode():
-#     qml.TShift(wires=0)
-#     qml.TShift(wires=1)
-#     qml.TAdd(wires=[0, 1])
+@qml.qnode(dev)
+def test_qnode():
+    qml.TShift(wires=0)
+    qml.TShift(wires=1)
+    qml.TAdd(wires=[0, 1])
 
-#     return qml.state()
-
-
-@qml.qnode(dev2)
-def test1():
-    qml.PauliX(wires=0)
-    qml.PauliY(wires=1)
-    qml.CNOT(wires=[1, 2])
-    return qml.sample(counts=True)
-
-
-@qml.qnode(dev2)
-def test2():
-    qml.PauliX(wires=0)
-    qml.Hadamard(wires=1)
-    qml.CNOT(wires=[1, 2])
-    return qml.sample()
+    return qml.state()
 
 
 if __name__ == "__main__":
-    print(test1())
-
-    print(test2())
+    print(test_qnode())
